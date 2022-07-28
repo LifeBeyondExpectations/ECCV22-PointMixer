613e79787ecf0b04bf5165e6a81a96855adee63d
jaesung.choe <jaesung.choe@kaist.ac.kr>
2022-07-28 12:44:52 +0000

    update s3dis test

--- sem_seg/script/run_nvidia_s3dis_00.sh
+++ sem_seg/script/run_nvidia_s3dis_00.sh
@@ -71,7 +71,8 @@ python train_pl.py \
   \
   --voxel_size 0.04  --eval_voxel_max 800000  --test_batch 1  --cudnn_benchmark False
 
-### TEST (pre-process stage for test dataset)
+### TEST (pre-process stage for test dataset) 
+### npts=800k
 python test_split_save.py \
   --MYCHECKPOINT $MYCHECKPOINT --computer $COMPUTER --shell $MYSHELL \
   --MASTER_ADDR $MASTER_ADDR \
@@ -98,6 +99,7 @@ python test_split_save.py \
   --voxel_size 0.04  --eval_voxel_max 800000  --test_batch 1  --cudnn_benchmark False
 
 ### TEST (evaluation)
+### npts=800k
 python test_pl.py \
   --MYCHECKPOINT $MYCHECKPOINT --computer $COMPUTER --shell $MYSHELL \
   --MASTER_ADDR $MASTER_ADDR \
@@ -122,5 +124,61 @@ python test_pl.py \
   --nsample 8 16 16 16 16  --drop_rate 0.1  --fea_dim 6  --classes 13 \
   \
   --voxel_size 0.04  --eval_voxel_max 800000  --test_batch 1
+
+#####
+
+### TEST (pre-process stage for test dataset) 
+### npts=40k
+python test_split_save.py \
+  --MYCHECKPOINT $MYCHECKPOINT --computer $COMPUTER --shell $MYSHELL \
+  --MASTER_ADDR $MASTER_ADDR \
+  --train_worker $WORKERS --val_worker $WORKERS \
+  --NUM_GPUS $NUM_GPUS  \
+  --train_batch $NUM_TRAIN_BATCH  \
+  --val_batch $NUM_VAL_BATCH  \
+  --test_batch $NUM_TEST_BATCH \
+  \
+  --scannet_train_root $SCANNET_TRAIN  --scannet_test_root $SCANNET_TEST \
+  --scannet_semgseg_root $SCANNET_SEMSEG \
+  --shapenet_root $SHAPENET  --shapenetcore_root $SHAPNETCORE \
+  --s3dis_root $S3DIS \
+  \
+  --neptune_proj $NEPTUNE_PROJ \
+  --epochs 60  --CHECKPOINT_PERIOD 1  --lr 0.1 \
+  --dataset $DATASET  --distributed_backend 'dp' --optim 'SGD' \
+  \
+  --model 'net_pointmixer' --arch $ARCH  \
+  --intraLayer $INTRALAYER  --interLayer $INTERLAYER \
+  --transdown  $TRANSDOWN --transup $TRANSUP \
+  --nsample 8 16 16 16 16  --drop_rate 0.1  --fea_dim 6  --classes 13 \
+  \
+  --voxel_size 0.04  --eval_voxel_max 40000  --test_batch 10  --cudnn_benchmark False
+
+### TEST (evaluation)
+### npts=40k
+python test_pl.py \
+  --MYCHECKPOINT $MYCHECKPOINT --computer $COMPUTER --shell $MYSHELL \
+  --MASTER_ADDR $MASTER_ADDR \
+  --train_worker $WORKERS --val_worker $WORKERS \
+  --NUM_GPUS $NUM_GPUS  \
+  --train_batch $NUM_TRAIN_BATCH  \
+  --val_batch $NUM_VAL_BATCH  \
+  --test_batch $NUM_TEST_BATCH \
+  \
+  --scannet_train_root $SCANNET_TRAIN  --scannet_test_root $SCANNET_TEST \
+  --scannet_semgseg_root $SCANNET_SEMSEG \
+  --shapenet_root $SHAPENET  --shapenetcore_root $SHAPNETCORE \
+  --s3dis_root $S3DIS \
+  \
+  --neptune_proj $NEPTUNE_PROJ \
+  --epochs 60  --CHECKPOINT_PERIOD 1  --lr 0.1 \
+  --dataset $DATASET  --distributed_backend 'dp' --optim 'SGD' \
+  \
+  --model 'net_pointmixer' --arch $ARCH  \
+  --intraLayer $INTRALAYER  --interLayer $INTERLAYER \
+  --transdown  $TRANSDOWN --transup $TRANSUP \
+  --nsample 8 16 16 16 16  --drop_rate 0.1  --fea_dim 6  --classes 13 \
+  \
+  --voxel_size 0.04  --eval_voxel_max 40000  --test_batch 10
 
 cd -
