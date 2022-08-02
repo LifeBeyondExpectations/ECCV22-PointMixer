a763c4b2a34b1a2aa6c241ba2cfe0d8ba6be3471
jaesung.choe <jaesung.choe@kaist.ac.kr>
2022-08-02 10:26:07 +0000

    init st-trans ScanNet

--- sem_seg/dataset/loader_scannet.py
+++ sem_seg/dataset/loader_scannet.py
@@ -13,13 +13,14 @@ import torch
 from torch.utils.data import Dataset
 
 # from util.data_util import sa_create, collate_fn
-from dataset.utils.data_util import data_prepare_v101 as data_prepare
+from dataset.utils.data_util import data_prepare_scannet as data_prepare
 from dataset.utils.data_util import sa_create
-from dataset.utils import transform as t
+from dataset.utils import transform_scannet as transform
+# from dataset.utils import transform as t
 # from dataset.utils.voxelize import voxelize
 
 seed=0
-# pl.seed_everything(seed) # , workers=True
+# pl.seed_everything(seed, workers=True)
 np.random.seed(seed)
 random.seed(seed)
 torch.manual_seed(seed)
@@ -62,204 +63,65 @@ class myImageFloder(Dataset):
     def __init__(self, args, mode, test_split=None):
         super().__init__()
 
-        self.mode = mode
-        self.data_root = deepcopy(args.scannet_semgseg_root)
-        self.voxel_size = float(args.voxel_size) # 0.04
-
         self.classes = float(args.classes) 
-        assert self.classes == 20
+        assert self.classes == 20, 'this is scannet v2'
+        self.ignore_label = int(args.ignore_label) # 255
 
-        self.aug = str(args.aug)
-        self.crop_npart = int(args.crop_npart)
+        self.mode = mode # self.split = split
+        data_root = deepcopy(args.scannet_semgseg_root) # self.data_root = data_root
+        self.voxel_size = float(args.voxel_size) # 0.04# self.voxel_size = voxel_size
+        # self.voxel_max = voxel_max
+        # self.transform = transform
+        # self.shuffle_index = shuffle_index
+        # self.loop = loop
 
-        if self.mode == 'train':
-            self.shuffle_index = True
+        if mode == "train" or mode == 'val' or mode == 'trainval':
+            # self.data_list = glob.glob(os.path.join(data_root, mode, "*.pth"))
+            self.data_list = glob.glob(os.path.join(data_root, "train", "*.pth")) + \
+                glob.glob(os.path.join(data_root, "val", "*.pth"))
             self.voxel_max = int(args.train_voxel_max)
+            self.transform = transform.Compose([
+                transform.RandomRotate(along_z=True),
+                transform.RandomScale(scale_low=0.8, scale_high=1.2),
+                transform.RandomDropColor(color_augment=0.0)
+            ])
+            self.shuffle_index = True
             self.loop = int(args.loop)
-
-            if self.aug == 'pointtransformer':
-                self.transform = t.Compose(
-                    [
-                        t.RandomScale([0.9, 1.1]), 
-                        t.ChromaticAutoContrast(), 
-                        t.ChromaticTranslation(), 
-                        t.ChromaticJitter(), 
-                        t.HueSaturationTranslation()
-                    ])
-            elif self.aug == 'pointtransformer-v2':
-                self.transform = t.Compose(
-                    [
-                        t.ChromaticAutoContrast(), 
-                        t.ChromaticTranslation(), 
-                        t.ChromaticJitter(), 
-                        t.HueSaturationTranslation()
-                    ])
-            elif self.aug == 'mink':
-                self.transform = t.Compose(
-                    [
-                        t.RandomHorizontalFlip('z'),
-                        t.ChromaticAutoContrast(),
-                        t.ChromaticTranslation(ratio=0.10),
-                        t.ChromaticJitter(std=0.05),
-                    ])
-            elif self.aug == 'elastic+mink':
-                self.transform = t.Compose(
-                    [
-                        t.ElasticDistortion(),
-                        t.RandomHorizontalFlip('z'),
-                        t.ChromaticAutoContrast(),
-                        t.ChromaticTranslation(ratio=0.10),
-                        t.ChromaticJitter(std=0.05),
-                    ])
-            elif self.aug == 'elastic+mink+crop':
-                self.transform = t.Compose(
-                    [
-                        t.ElasticDistortion(),
-                        t.CoordCrop(npart=self.crop_npart), # jaesungchoe
-                        t.RandomHorizontalFlip('z'),
-                        t.ChromaticAutoContrast(),
-                        t.ChromaticTranslation(ratio=0.10),
-                        t.ChromaticJitter(std=0.05),
-                    ])
-            elif self.aug == 'mink+crop':
-                self.transform = t.Compose(
-                    [
-                        t.CoordCrop(npart=self.crop_npart), # jaesungchoe
-                        t.RandomHorizontalFlip('z'),
-                        t.ChromaticAutoContrast(),
-                        t.ChromaticTranslation(ratio=0.10),
-                        t.ChromaticJitter(std=0.05),
-                    ])
-            else:
-                raise NotImplemented
-            self.load_train_data()
-        
-            # elif self.mode == 'trainval':
-            #     self.shuffle_index = True
-            #     self.voxel_max = int(args.train_voxel_max) # 40000
-            #     self.loop = int(args.loop)
-            #     self.transform = t.Compose(
-            #         [
-            #             t.RandomScale([0.9, 1.1]), 
-            #             t.ChromaticAutoContrast(), 
-            #             t.ChromaticTranslation(), 
-            #             t.ChromaticJitter(), 
-            #             t.HueSaturationTranslation()
-            #         ])
-            #     self.load_trainval_data()
-
-        elif self.mode == 'val':
-            self.shuffle_index = False
-            self.voxel_max = int(args.eval_voxel_max) # 800000
-            self.loop = 1
-            self.transform = None
-                
-            self.load_val_data()
-
-        elif self.mode == 'test':
-            self.shuffle_index = False
+        elif mode == 'test':
+            self.data_list = glob.glob(os.path.join(data_root, mode, "*.pth"))
             self.voxel_max = int(args.eval_voxel_max) # 40000
-            self.loop = 1
             self.transform = None
+            self.shuffle_index = False
+            self.loop = 1
 
             self.test_split = test_split
             assert self.test_split is not None
-            
-            self.load_test_data()
-
         else:
-            raise NotImplemented
+            raise ValueError("no such mode: {}".format(mode))
+
+        ### DEBUG
+        # for idx in range(self.__len__()):
+        #     print(f'idx[{idx:4d}]')
+        #     self.__getitem__(idx=idx)
+
+    def __len__(self):
+        return len(self.data_list) * self.loop
 
     def __getitem__(self, idx):
 
-        if self.mode in ['train', 'val', 'trainval']:
-            idx = idx % len(self.data_paths)
-            filepath = self.data_paths[idx]
-            plydata = PlyData.read(filepath)
-            data = plydata.elements[0].data
-            coord = np.array([data['x'], data['y'], data['z']], dtype=np.float32).T
-            feat = np.array([data['red'], data['green'], data['blue']], dtype=np.float32).T
-            label = np.array(data['label'], dtype=np.int32)
-            # feat.max() == 255            
-            # print("mode[%s], idx[%d], coord[%d]"%(self.mode, idx, coord.shape[0])); return
-            coord, feat, label = data_prepare(
-                coord, feat, label, 
-                self.mode, self.voxel_size, self.voxel_max, 
-                self.transform, self.shuffle_index)
-            # feat.max() == 1.0
+        data_idx = idx % len(self.data_list)
+        data_path = self.data_list[data_idx]
+        data = torch.load(data_path)
 
-            # pdb.set_trace()
-            # pcd = o3d.geometry.PointCloud()
-            # pcd.points = o3d.utility.Vector3dVector(coord)
-            # pcd.colors = o3d.utility.Vector3dVector(feat)
-            # o3d.io.write_point_cloud('/root/scannet_sampled_input.ply', pcd)
+        coord, feat, label = data[0], data[1], data[2]
 
-            return coord, feat, label
-
-        elif (self.mode == 'test'):
-            data_idx = self.data_idx[idx]
-            data_path = self.data_list[data_idx]
-            data = np.load(data_path) 
-
-            pred_idx = data['idx_part']
-            coord = data['coord_part']
-            feat = data['feat_part'] # feat.max() == 255
-            label = data['label_part']
-            offset = data['offset_part']
-
-            coord, feat, label = data_prepare(
-                coord, feat, label, 
-                split=None, voxel_size=None, voxel_max=None, 
-                transform=None, shuffle_index=None)
-            
-            pred_idx = torch.LongTensor(pred_idx)
-
-            return coord, feat, label, pred_idx, offset
-
-        else:
-            raise NotImplemented
-
-    def load_train_data(self):
-        scenes = self.read_txt(os.path.join(self.data_root, "scannetv2_train.txt"))        
-        self.data_paths = [os.path.join(self.data_root, 'train/%s.ply'%(scene)) for scene in scenes]
-        print("Totally {} samples in {} set.".format(len(self.data_paths), self.mode))
-    
-    def load_val_data(self):
-        scenes = self.read_txt(os.path.join(self.data_root, "scannetv2_val.txt"))
-        self.data_paths = [os.path.join(self.data_root, 'train/%s.ply'%(scene)) for scene in scenes]
-        print("Totally {} samples in {} set.".format(len(self.data_paths), self.mode))
-
-    def load_trainval_data(self):
-        scenes_train = self.read_txt(os.path.join(self.data_root, "scannetv2_train.txt"))        
-        scenes_val = self.read_txt(os.path.join(self.data_root, "scannetv2_val.txt"))
-        scenes = scenes_train + scenes_val
-        self.data_paths = [os.path.join(self.data_root, 'train/%s.ply'%(scene)) for scene in scenes]
-        print("Totally {} samples in {} set.".format(len(self.data_paths), self.mode))
-    
-    def load_test_data(self):
-        filename = self.test_split + '__' + '*__npts_{:09d}__size0p{:04d}.npz'.format(
-            self.voxel_max, int(self.voxel_size*10000))
-        self.data_list = sorted(glob.glob(os.path.join(self.data_root, 'val_split', filename)))
-        self.data_idx = np.arange(len(self.data_list))
-        print("Totally {} samples in {} set.".format(len(self.data_idx), self.mode))
+        label[label == -100] = self.ignore_label
         
-        # DO NOT ERASE 
-        #
-        # scenes = self.read_txt(os.path.join(self.data_root, "scannetv2_test.txt"))
-        # self.data_paths = [os.path.join(self.data_root, 'test/%s.ply'%(scene)) for scene in scenes]
-        # # self.data_paths = sorted(glob.glob(os.path.join(self.data_root, 'test/*.ply')))
-        # print("Totally {} samples in {} set.".format(len(self.data_paths), self.mode))       
+        coord, feat, label = data_prepare(
+            coord, feat, label, 
+            self.mode, self.voxel_size, self.voxel_max, 
+            self.transform, self.shuffle_index)
+        
+        return coord, feat, label
 
-    def __len__(self):
-        if self.mode in ['train', 'val', 'trainval']:
-            return len(self.data_paths) * self.loop
-        elif (self.mode == 'test'):
-            return len(self.data_idx) * self.loop
-        else:
-            raise NotImplemented
-    
-    def read_txt(self, path):
-        with open(path) as f:
-            lines = f.readlines()
-        lines = [x.strip() for x in lines]
-        return lines
+
--- sem_seg/dataset/utils/data_util.py
+++ sem_seg/dataset/utils/data_util.py
@@ -95,3 +95,30 @@ def data_prepare_v101(
     
     return coord, feat, label
 
+def data_prepare_scannet(
+    coord, feat, label, 
+    split='train', voxel_size=0.04, voxel_max=None, 
+    transform=None, shuffle_index=False):
+    if transform:
+        # coord, feat, label = transform(coord, feat, label)
+        coord, feat = transform(coord, feat)
+    if voxel_size:
+        coord_min = np.min(coord, 0)
+        coord -= coord_min
+        uniq_idx = voxelize(coord, voxel_size)
+        coord, feat, label = coord[uniq_idx], feat[uniq_idx], label[uniq_idx]
+    if voxel_max and label.shape[0] > voxel_max:
+        init_idx = np.random.randint(label.shape[0]) if 'train' in split else label.shape[0] // 2
+        crop_idx = np.argsort(np.sum(np.square(coord - coord[init_idx]), 1))[:voxel_max]
+        coord, feat, label = coord[crop_idx], feat[crop_idx], label[crop_idx]
+    if shuffle_index:
+        shuf_idx = np.arange(coord.shape[0])
+        np.random.shuffle(shuf_idx)
+        coord, feat, label = coord[shuf_idx], feat[shuf_idx], label[shuf_idx]
+
+    coord_min = np.min(coord, 0)
+    coord -= coord_min
+    coord = torch.FloatTensor(coord)
+    feat = torch.FloatTensor(feat)
+    label = torch.LongTensor(label)
+    return coord, feat, label
